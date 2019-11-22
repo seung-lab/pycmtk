d703ca768875f20bdb233aa2fac9cdcb8faf69e7
William Silversmith <william.silversmith@gmail.com>
2019-11-22 14:41:28 -0500

    fix: remove empty directory creation

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ from shutil import copyfile
 from setuptools import setup, Distribution
 import subprocess
 
-if not os.path.isdir('pycmtk'):
-    os.mkdir('pycmtk')
-
 # c++ -O3 -Wall -shared -std=c++11 -fPIC `python3 -m pybind11 --includes` example.cpp -o example`python3-config --extension-suffix`
 
 extension = str(subprocess.check_output('python3-config --extension-suffix', shell=True))[2:-3]
